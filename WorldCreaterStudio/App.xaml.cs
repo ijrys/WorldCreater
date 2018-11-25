e4aad56c910a5dfd675a5f918186bae02cfe6e16
ijrys <hanqindong@outlook.com>
2018-11-25 22:34:10 +0800

    Change WPF version to Studio

--- WorldCreaterStudio/App.xaml.cs
+++ WorldCreaterStudio/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace ControlPanelWPF {
+namespace WorldCreaterStudio {
 	/// <summary>
 	/// App.xaml 的交互逻辑
 	/// </summary>
